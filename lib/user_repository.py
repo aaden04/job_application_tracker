14376c8a0e47b18585d6a358a41a18fa3d4f7087
aaden04 <mraaden10@gmail.com>
2025-02-11 19:17:46 +0000

    user_repo plus test

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -2,3 +2,57 @@ from lib.database_connection import DatabaseConnection
 import hashlib
 from lib.user import *
 
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def create(self, name, email, phone_number, password):
+        binary_password = password.encode("utf-8")
+        hashed_password = hashlib.sha256(binary_password).hexdigest()
+        self._connection.execute(
+            'INSERT INTO users (name, email, phone_number, hashed_password) VALUES (%s, %s, %s, %s)', 
+            [name.title(), email.lower(), phone_number.strip(), hashed_password]
+        )
+
+    def check_password(self, email, password_attempt):
+        binary_password_attempt = password_attempt.encode("utf-8")
+        hashed_password_attempt = hashlib.sha256(binary_password_attempt).hexdigest()
+        rows = self._connection.execute(
+            "SELECT * FROM users WHERE email = %s AND hashed_password = %s", 
+            [email, hashed_password_attempt]
+        )
+        return len(rows) > 0
+
+    def find_by_email(self, email):
+        rows = self._connection.execute("SELECT * FROM users WHERE email = %s", [email])
+        id = rows[0]['id']
+        hashed_password = rows[0]['hashed_password']
+        phone_number = rows[0]['phone_number']
+        name = rows[0]['name']
+        user = User(id, name, email, phone_number, hashed_password)
+        return user
+
+    def all(self):
+        rows = self._connection.execute('SELECT * FROM users')
+        users = []
+        for row in rows:
+            person = User(
+                row["id"],
+                row["name"],
+                row["email"],
+                row["phone_number"],
+                row["hashed_password"]
+            )
+            users.append(person)
+        return users
+
+    def find(self, user_id):
+        result = self._connection.execute("SELECT * FROM users WHERE id = %s", [user_id])
+        row = result[0]
+        return User(
+            row["id"], 
+            row["name"], 
+            row["email"], 
+            row["phone_number"], 
+            row["hashed_password"]
+        )
---
+++ tests/test_user_repository.py
@@ -0,0 +1,26 @@
+
+from lib.user_repository import UserRepository
+from lib.user import User
+
+def test_user_repository_gets_all_records(db_connection):
+    db_connection.seed("seeds/Jobs.sql")
+    repository = UserRepository(db_connection)
+    users = repository.all()
+    assert len(users) == 6
+    assert users[0].name == "John"
+    assert users[0].email == "john@gmail.com"
+
+def test_create_user(db_connection):
+    db_connection.seed("seeds/Jobs.sql")
+    repository = UserRepository(db_connection)
+
+    repository.create("Bobby", "bobby@gmail.com", "07946477898", "qwerty123")
+    
+    result = repository.all()
+    assert len(result) == 7
+    new_user = result[-1]
+    
+    assert new_user.name == "Bobby"
+    assert new_user.email == "bobby@gmail.com"
+    assert new_user.phone_number == "07946477898"
+    assert new_user.password == "qwerty123"
