e71610dc25af0fb4b10497bff984a5bf28a7210e
Mahmoud <mahmoudmoe53@hotmail.com>
2025-02-12 12:24:52 +0000

    corrected homepage routes

--- app.py
+++ app.py
@@ -2,17 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="frontend/templates")
 
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        return f"User {username} signed up successfully!"
-
-    return render_template('signup.html')
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -25,6 +15,18 @@ def login():
             return "Invalid credentials, please try again."
 
     return render_template('login.html')
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        return f"User {username} signed up successfully!"
+
+    return render_template('signup.html')
 
 
 if __name__ == '__main__':
