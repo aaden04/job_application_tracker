d871a3e2421a07502a3eb303bfdf903702e91443
aaden04 <mraaden10@gmail.com>
2025-02-18 19:41:58 +0000

    tests are passing again

--- lib/job_application_repository.py
+++ lib/job_application_repository.py
@@ -27,3 +27,19 @@ class ApplicationsRepository:
                 [company, title, location, salary, date_applied, application_id, user_id]
             )
         self._connection.commit()  
+
+    def find_by_company_name(self, company_name, user_id):
+        with self._connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM applications WHERE company = %s AND user_id = %s",
+                [company_name, user_id]
+            )
+            return cursor.fetchall()
+
+    def find_by_job_title(self, title, user_id):
+        with self._connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM applications WHERE title = %s AND user_id = %s",
+                [title, user_id]
+            )
+            return cursor.fetchall()
